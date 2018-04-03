3310f5d3ab323237c17cb09e58ef31c69f2e0454
Jane <jane@D-10-16-31-53.dhcp4.washington.edu>
2018-04-03 14:50:36 -0700

    Added logging.

--- app/src/main/java/edu/tacoma/uw/css/cmarsten/myapplication/MainActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/cmarsten/myapplication/MainActivity.java
@@ -2,12 +2,17 @@ package edu.tacoma.uw.css.cmarsten.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "Information about onCreate");
     }
 }
